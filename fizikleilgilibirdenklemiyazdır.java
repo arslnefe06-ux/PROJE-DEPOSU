03f499be3c4b57e73ddcca37c88b2bd7d4b5a676
arslnefe06-ux <arsln.efe.06@gmail.com>
2025-12-12 11:01:22 +0300

    Add files via upload
    Fizikle ilgili bir denklemi koda dökülmüş hali

---
+++ fizikleilgilibirdenklemiyazdır.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Efe
+ */
+import java.util.Scanner;
+public class fizikleilgilibirdenklemiyazdır {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir m ve a değeri gir:");
+        int m = input.nextInt();
+        int a = input.nextInt();
+        
+        double F = (double) m*a;
+
+        System.out.println("Kuvvet:"+F);            
+             
+    }
+}
